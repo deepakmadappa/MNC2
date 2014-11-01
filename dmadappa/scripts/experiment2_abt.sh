24966161c4b56ca99a7c27ed813493b303081817
deepakmadappa <deepumadappa@gmail.com>
2014-11-01 16:50:46 -0400

    added experiments scripts

---
+++ dmadappa/scripts/experiment2_abt.sh
@@ -0,0 +1,7 @@
+cd $HOME/MNC2/dmadappa/scripts
+
+./run_experiments -m1000 -l0.2 -c0.2 -t50 -p../sr -w10
+
+./run_experiments -m1000 -l0.5 -c0.2 -t50 -p../sr -w10
+
+./run_experiments -m1000 -l0.8 -c0.2 -t50 -p../sr -w10
---
+++ dmadappa/scripts/experiment2_gbn.sh
@@ -0,0 +1,19 @@
+cd $HOME/MNC2/dmadappa/scripts
+
+./run_experiments -m1000 -l0.2 -c0.2 -t50 -p../gbn -w10
+./run_experiments -m1000 -l0.2 -c0.2 -t50 -p../gbn -w50
+./run_experiments -m1000 -l0.2 -c0.2 -t50 -p../gbn -w100
+./run_experiments -m1000 -l0.2 -c0.2 -t50 -p../gbn -w200
+./run_experiments -m1000 -l0.2 -c0.2 -t50 -p../gbn -w500
+
+./run_experiments -m1000 -l0.5 -c0.2 -t50 -p../gbn -w10
+./run_experiments -m1000 -l0.5 -c0.2 -t50 -p../gbn -w50
+./run_experiments -m1000 -l0.5 -c0.2 -t50 -p../gbn -w100
+./run_experiments -m1000 -l0.5 -c0.2 -t50 -p../gbn -w200
+./run_experiments -m1000 -l0.5 -c0.2 -t50 -p../gbn -w500
+
+./run_experiments -m1000 -l0.8 -c0.2 -t50 -p../gbn -w10
+./run_experiments -m1000 -l0.8 -c0.2 -t50 -p../gbn -w50
+./run_experiments -m1000 -l0.8 -c0.2 -t50 -p../gbn -w100
+./run_experiments -m1000 -l0.8 -c0.2 -t50 -p../gbn -w200
+./run_experiments -m1000 -l0.8 -c0.2 -t50 -p../gbn -w500
